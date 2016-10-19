628d5f208fae314224b1a85049ff32721171b487
michaelzhang <michaelzhang@aegonthtf.com>
2016-10-19 16:06:27 +0800

    v1

--- src/main/java/com/spring/SpringlearningApplication.java
+++ src/main/java/com/spring/SpringlearningApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringlearningApplication {
 
 	public static void main(String[] args) {
+		System.out.println("dddd");
 		SpringApplication.run(SpringlearningApplication.class, args);
 	}
 }
