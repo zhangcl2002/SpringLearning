93c359d787b2f9fad941d40ada663d7de208ec7d
michaelzhang <michaelzhang@aegonthtf.com>
2016-10-20 13:15:07 +0800

    v1

---
+++ src/main/java/com/spring/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void hello(){
+		System.out.println("hello " + name);
+	}
+	
+	
+
+}
--- src/main/java/com/spring/SpringlearningApplication.java
+++ src/main/java/com/spring/SpringlearningApplication.java
@@ -1,13 +1,31 @@
 package com.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringlearningApplication {
 
+
+@SpringBootApplication
+public class SpringlearningApplication implements CommandLineRunner {
+
+	@Autowired
+	private HelloWorld helloworld;
+				
 	public static void main(String[] args) {
 		System.out.println("dddd");
 		SpringApplication.run(SpringlearningApplication.class, args);
+									
+	}
+
+
+
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		helloworld.setName("michael");
+		helloworld.hello();
+		
 	}
 }
